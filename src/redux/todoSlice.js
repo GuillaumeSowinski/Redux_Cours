25d4f1dda72f885e8e078001ec5c753107760e14
Guillaume <guillaume@sowinski.fr>
2025-10-23 10:42:49 +0200

    Immer test

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,25 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const initialState = [];
 
 const todoSlice = createSlice({
     name: "todos",
     initialState,
     reducers: {
-        addTodo: (state, action) => {
-            return [
-                ...state,
-                { id: Date.now(), text: action.payload, completed: false },
-            ];
-        },
-        toggleTodo: (state, action) => {
-            return state.map((todo) =>
-                todo.id === action.payload
-                    ? { ...todo, completed: !todo.completed }
-                    : todo
-            );
-        },
+        addTodo: (draft, action) => {
+            draft.push({ id: Date.now(), text: action.payload, completed: false });
     },
+        toggleTodo: (draft, action) => {
+            const todo = draft.find((task) => task.id === action.payload);
+            if (todo) {
+                todo.completed = !todo.completed;
+      }
+    },
+  },
 });
 
 export const { addTodo, toggleTodo } = todoSlice.actions;
